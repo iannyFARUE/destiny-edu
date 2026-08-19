dac4b9eba33ac31a6e50c25221e967f2e560c526
Ian Farai Madhara <iannyfarai@gmail.com>
2026-08-18 23:58:08 -0400

    added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'app-root',
   standalone: false,
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'destiny-edu';
