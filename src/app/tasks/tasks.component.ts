afd9c03ce15142191fc6d6d3637ff759d8f0106d
Ian Farai Madhara <iannyfarai@gmail.com>
2026-08-24 23:35:32 -0400

    added task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { dummyTasks } from '../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -9,4 +10,11 @@ import { TaskComponent } from './task/task.component';
 })
 export class TasksComponent {
   @Input({ required: true }) name!: string;
+  @Input({ required: true }) userId!: string;
+
+  tasks = dummyTasks;
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-// type User = { id: string; name: string; avatar: string };
-
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
